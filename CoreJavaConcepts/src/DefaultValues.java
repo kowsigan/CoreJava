2f31ad645a1a6f666d456f766c9028ef8e9f6158
kowsigan <b.kowsigan@gmail.com>
2016-12-02 21:12:40 +0530

    Initial default values commit

---
+++ CoreJavaConcepts/src/DefaultValues.java
@@ -0,0 +1,39 @@
+class test
+{
+	int a;
+	float f;
+	double d;
+	char c;
+	String s;
+	boolean b;
+	
+}
+	
+
+
+
+public class DefaultValues {
+
+	
+	
+	public DefaultValues()
+	{
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		test dv = new test();	
+		
+		
+		System.out.println(dv.a  );
+		
+		System.out.println( (int)dv.c );
+		
+		
+		
+
+	}
+
+}
